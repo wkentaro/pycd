1510c5056c672b21101565cf37e99695facfefee
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-04-20 06:28:12 +0900

    update pycd.sh

--- pycd/pycd.sh
+++ pycd/pycd.sh
@@ -3,14 +3,15 @@
 
 function pycd ()
 {
-  module_path=$(pycd_py find $1 --no-warning)
+  local module_path
+  module_path=`pycd_py find $1 --no-warning`
   if [ "$module_path" != "" ]; then
     cd $module_path
   else
     if [ "$1" = "" ]; then
       echo "Usage: pycd <module_name>"
     else
-      echo "$1 not found"
+      echo "not found"
     fi
   fi
 }
