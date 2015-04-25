409dbfbb02f28f1a9c33ebc87ad909d81f5ba48d
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-04-25 13:13:44 +0900

    0.1.21

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if sys.argv[-1] == 'publish':
 
 long_desc = """Simple command line tool to change directory
 for python modules. You can now easily read the codes of the modules."""
-version='0.1.20'
+version='0.1.21'
 
 
 setup(
