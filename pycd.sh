2f8c015d37343b0b5abd84f28e790431606e5dc3
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-29 15:09:12 +0900

    Fix: check pycd is installed

--- pycd.sh
+++ pycd.sh
@@ -3,15 +3,21 @@
 
 function pycd ()
 {
-  local module_path
-  module_path=`pypkg find $1 --no-warning`
-  if [ "$module_path" != "" ]; then
-    cd $module_path
+  local pkg_path
+  # check if command installed
+  which pypkg >/dev/null 2>&1 || {
+    echo "please install pycd"
+    echo "run \`pip install pycd\`"
+    return 1
+  }
+  if [ "$1" = "" ]; then
+    echo "Usage: pycd <module_name>"
   else
-    if [ "$1" = "" ]; then
-      echo "Usage: pycd <module_name>"
-    else
+    pkg_path=$(pypkg find $1 --no-warning)
+    if [ "$pkg_path" = "" ]; then
       echo "$1 not found"
+    else
+      cd $pkg_path
     fi
   fi
 }
