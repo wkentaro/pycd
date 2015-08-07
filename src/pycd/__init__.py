02039f0406954fad0666e464877fbd9633218a69
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-08-07 14:57:18 +0900

    Get current PYTHONPATH in main()

--- src/pycd/__init__.py
+++ src/pycd/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+import os
+import sys
+
 from .cli import *
 from .package import *
 from .__version__ import version as __version__
+
+
+# get current PYTHONPATH
+sys.path = os.getenv('PYTHONPATH').split(':') + sys.path
